03d78bc56bb222e49a9c168c7f8f1390331571cc
Xavier Bouclet <mikrethor@gmail.com>
2024-12-05 07:51:57 -0500

    DAY04 - Advent of Spring Boot - Error Handling

---
+++ 20241204/src/main/java/com/xavierbouclet/adventofcode/RouterConfiguration.java
@@ -0,0 +1,26 @@
+package com.xavierbouclet.adventofcode;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import static org.springframework.web.servlet.function.RouterFunctions.route;
+
+@Configuration(proxyBeanMethods = false)
+public class RouterConfiguration {
+
+    @Bean
+    public RouterFunction<ServerResponse> customerRouter(CustomerHandler customerHandler) {
+        return route()
+                .path("/api/customers/{id}", builder -> builder
+                        .GET(customerHandler::getCustomer)
+                        .DELETE(customerHandler::deleteCustomer)
+                )
+                .path("/api/customers", builder -> builder
+                        .GET(req -> customerHandler.getCustomers())
+                        .POST(customerHandler::createCustomer)
+                )
+                .build();
+    }
+}
