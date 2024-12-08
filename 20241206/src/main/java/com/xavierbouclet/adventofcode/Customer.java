9229cb2cf38ae3c14584a2bd0630b68d5bc1f5c0
Xavier Bouclet <mikrethor@gmail.com>
2024-12-07 21:18:41 -0500

    DAY06 - Advent of Spring Boot - ProblemDetail

---
+++ 20241206/src/main/java/com/xavierbouclet/adventofcode/Customer.java
@@ -0,0 +1,6 @@
+package com.xavierbouclet.adventofcode;
+
+import java.util.UUID;
+
+public record Customer(UUID id, String firstName, String lastName) {
+}
---
+++ 20241206/src/main/java/com/xavierbouclet/adventofcode/CustomerHandler.java
@@ -0,0 +1,45 @@
+package com.xavierbouclet.adventofcode;
+
+import jakarta.servlet.ServletException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.function.ServerRequest;
+import org.springframework.web.servlet.function.ServerResponse;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Component
+public class CustomerHandler {
+
+    private List<Customer> customers = new ArrayList<>(List.of(
+            new Customer(UUID.randomUUID(), "Santa", "Claus"),
+            new Customer(UUID.randomUUID(), "Maria", "Carey")));
+
+    public ServerResponse getCustomers() {
+        return ServerResponse.ok().body(this.customers);
+    }
+
+    public ServerResponse getCustomer(ServerRequest request) {
+        UUID id = UUID.fromString(request.pathVariable("id"));
+        return ServerResponse.ok().body(this.customers.stream().filter(p -> p.id().equals(id)).findFirst().orElseThrow(() -> new ResourceNotFoundException()));
+    }
+
+    public ServerResponse createCustomer(ServerRequest request) throws ServletException, IOException {
+        Customer customer = request.body(Customer.class);
+
+        var customerToCreate = new Customer(UUID.randomUUID(), customer.firstName(), customer.lastName());
+
+        this.customers.add(customerToCreate);
+
+        return ServerResponse.ok().body(customerToCreate);
+    }
+
+    public ServerResponse deleteCustomer(ServerRequest request) {
+        UUID id = UUID.fromString(request.pathVariable("id"));
+        this.customers.removeIf(person -> person.id().equals(id));
+        return ServerResponse.noContent().build();
+    }
+
+}
